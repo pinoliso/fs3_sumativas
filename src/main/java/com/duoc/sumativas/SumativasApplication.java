426f6ed5b38fd855084203b3cab732379fb7b461
Enrique Pino <pinoliso@gmail.com>
2024-10-25 22:56:07 -0300

    base

---
+++ src/main/java/com/duoc/sumativas/SumativasApplication.java
@@ -0,0 +1,13 @@
+package com.duoc.sumativas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SumativasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SumativasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/duoc/sumativas/SumativasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duoc.sumativas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SumativasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
